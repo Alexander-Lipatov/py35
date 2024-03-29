59e4d28bfcd1d16f1cf9b4806ace6c1e7789c5eb
alexander-lipatov <lap1993.12@yandex.ru>
2024-03-29 20:58:21 +0300

    dz_1

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print(102)
+print(
+    "“Life is what happens", "\twhen",
+    "\t\tyou’re busy making other plans”",
+    "\t\t\tJohn Lennon.",
+    sep="\n"
+)
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter a number: ")
+# num3 = input("Enter a number: ")
+# num4 = input("Enter a number: ")
+# print(int(num1+num2+num3+num4))
+
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a number: "))
+# num3 = int(input("Enter a number: "))
+# num4 = int(input("Enter a number: "))
+# print(num1*num2*num3*num4)
+
+
+# nums = input("Enter a list of numbers: ").split(" ")
+# result = None
+# for num in nums:
+#     if result is None:
+#         result = int(num)
+#     result *= int(num)
+# print(result)
+
+
+# metre = input("Enter a metre: ")                 
+
+# print("Сантиметры", float(metre) * 100)         
+# print("Миллиметры", float(metre) * 100 * 10)    
+# print("Миль", float(metre) * 0.000621)    
+
+
+# square_triangle = None
+# footing = input("Enter a footing: ")
+# haight = input("Enter a haight: ")
+# square_triangle = float(footing) * float(haight) * 0.5
+# print(square_triangle)
+
+# four_digit_number = input("Enter a four digit number: ")
+# res = ''
+# for digit in four_digit_number[::-1]:
+#     res += digit
+# print(res)
+
+
