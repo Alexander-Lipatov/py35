7304aaf0438596adc3f78a6957397c2f02bddf82
alexander-lipatov <lap1993.12@yandex.ru>
2024-03-29 21:34:06 +0300

    commit

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
-print(
-    "“Life is what happens", "\twhen",
-    "\t\tyou’re busy making other plans”",
-    "\t\t\tJohn Lennon.",
-    sep="\n"
-)
+nums = input("Enter a numbers: ").split()
+res = None
 
-# num1 = input("Enter a number: ")
-# num2 = input("Enter a number: ")
-# num3 = input("Enter a number: ")
-# num4 = input("Enter a number: ")
-# print(int(num1+num2+num3+num4))
-
-
-# num1 = int(input("Enter a number: "))
-# num2 = int(input("Enter a number: "))
-# num3 = int(input("Enter a number: "))
-# num4 = int(input("Enter a number: "))
-# print(num1*num2*num3*num4)
-
-
-# nums = input("Enter a list of numbers: ").split(" ")
-# result = None
-# for num in nums:
-#     if result is None:
-#         result = int(num)
-#     result *= int(num)
-# print(result)
-
-
-# metre = input("Enter a metre: ")                 
-
-# print("Сантиметры", float(metre) * 100)         
-# print("Миллиметры", float(metre) * 100 * 10)    
-# print("Миль", float(metre) * 0.000621)    
-
-
-# square_triangle = None
-# footing = input("Enter a footing: ")
-# haight = input("Enter a haight: ")
-# square_triangle = float(footing) * float(haight) * 0.5
-# print(square_triangle)
-
-# four_digit_number = input("Enter a four digit number: ")
-# res = ''
-# for digit in four_digit_number[::-1]:
-#     res += digit
-# print(res)
-
-
+try:
+    choice = int(input("Выбери вариант: \n"
+                       "1.Сумма \n" +
+                       "2.Произведение \n" +
+                       "3.Максимальное \n"
+                       "4.Минимальное \n"
+                       "5.Среднее \n")
+                 )
+    for num in nums:
+        if res is None:
+            res = int(num)
+            continue
+        elif choice == 1:
+            res += int(num)
+        elif choice == 2:
+            res *= int(num)
+        elif choice == 3:
+            res = max(res, int(num))
+        elif choice == 4:
+            res = min(res, int(num))
+        elif choice == 5:
+            res += int(num)
+            res = res / len(nums)
+    print(res)
+except Exception:
+    print("Введите корректный вариант!")
