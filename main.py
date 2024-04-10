ac779f483e8cc71029cf2c12ac88e6a637e0a8b0
alexander-lipatov <lap1993.12@yandex.ru>
2024-04-10 21:14:18 +0300

    1

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+
+import string
+import random
+
+import re
+
+
 def polindrom():
     str = input('Input text: ').replace(" ", "").lower()
     if str == str[::-1]:
@@ -7,6 +14,17 @@ def polindrom():
 
 
 
+
 def upper_for_word():
     str = input('Input text: ')
-    str = input('Input text: ')
+    words = input('Input words: ')
+
+    words = words.split()
+    for word in words:
+         str = str.replace(word, word.upper())
+    print(str)
+
+upper_for_word()
+
+
+print()
