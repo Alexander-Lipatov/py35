d9b84ebef66821485683c4d31bf6f4cc8527b9cc
alexander-lipatov <lap1993.12@yandex.ru>
2024-04-19 21:17:17 +0300

    1

--- main.py
+++ main.py
@@ -107,13 +107,11 @@ def PZ_04_3_task1():
             negative_sum += i
             positive_el = []
             positive_count = 0
-        
+
         if i > 0:
-            positive_count+=1
-            if positive_count > 1: 
+            positive_count += 1
+            if positive_count > 1:
                 positive_el.append(i)
-                
-        
 
     print(negative_sum)
     print(even_sum)
@@ -127,14 +125,14 @@ def PZ_04_3_task1():
 PZ_04_3_task1()
 
 
-def func(i:int, b: bool)-> list:
+def func(i: int, b: bool) -> list:
     return [i, b]
 
 
 print(func('1', True))
 
 
-def func2(i:int, b: bool)-> list:
+def func2(i: int, b: bool) -> list:
     return [i, b]
 
 
@@ -152,12 +150,13 @@ def text_Bill_Gates():
           if you do so, you are insulting yourself.”
                                                     Bill Gates
           ''')
-    
+
 # text_Bill_Gates()
 
 # Напишите функцию, которая принимает два числа
 # в качестве параметра и отображает все четные числа
 # между ними.
+
 
 def func3(a: int, b: int) -> None:
     for i in range(a, b + 1):
@@ -175,25 +174,153 @@ def func3(a: int, b: int) -> None:
 # ■ если она равна True, квадрат заполненный;
 # ■ если False, квадрат пустой.
 
-def square(lenght:int, char:str, is_empty: bool)-> str:
+
+def square(lenght: int, char: str, is_empty: bool) -> str:
     print(lenght)
     string = ""
-    for y in range(lenght):
-        for x in range(lenght):
-            if is_empty:
-                if y==lenght-1:
-                    string += ' '+char+' '
-                elif y == 0:
-                    string += ' '+char+' '
-            else:
-                string += ' '+char+' '
-        string+='\n'
+    for x in range(l):
+        for y in range(l):
+            pass
 
-                
     print(string)
-square(11, "*", True)
 
 
+# square(11, "*", True)
+
+# Напишите функцию, которая возвращает минимальное
+# из пяти чисел. Числа передаются в качестве параметров.
 
 
+def min_five_number(*args):
+    numbers = input('input numbers... : ').split(' ')
+    min = args[0]
+    for i in args[:6:]:
+        if i < min:
+            min = i
+    return min
 
+
+# print(min_five_number(6,4,66,4,7,3123,123,23))
+
+def range_params(args):
+    if args[0] > args[1]:
+        start = args[1]
+        end = args[0]
+    else:
+        start = args[0]
+        end = args[1]
+    return start, end
+
+
+def composition_numbers(*args):
+    result = 1
+    start, end = range_params(args)
+    for i in range(start, end+1):
+        result *= i
+
+    return result
+
+
+res = composition_numbers(10, 5)
+
+# print(res)
+
+
+def count_numbers(number):
+    return len(str(number))
+
+# print(count_numbers(1234))
+
+
+def number_polindrom(number):
+    if str(number) == str(number)[::-1]:
+        return True
+    return False
+
+
+print(number_polindrom(123431))
+
+# Напишите функцию, вычисляющую произведение
+# элементов списка целых. Список передаётся в качестве па-
+# раметра. Полученный результат возвращается из функции.
+
+
+def composition_numbers_2(nums_list=[]):
+    result = 1
+
+    for i in nums_list:
+        result *= i
+
+    return result
+
+# print(composition_numbers_2([5,6,7,8,9,10]))
+
+
+# Напишите функцию для нахождения минимума в
+# списке целых. Список передаётся в качестве параметра.
+# Полученный результат возвращается из функции.
+
+def min_number(nums_list=[]):
+    min = nums_list[0]
+    for i in nums_list:
+        if i < min:
+            min = i
+    return min
+
+# print(min_number([6,5,4,3,2,3,4,5,6]))
+
+# Напишите функцию, определяющую количество про-
+# стых чисел в списке целых. Список передаётся в качестве
+# параметра. Полученный результат возвращается из функции.
+
+
+def simple_list(list):
+    lst = []
+    k = 0
+    count = 0
+    for num in list:
+        for i in range(2, num):
+            if num % i == 0:
+                k = k+1
+        if k==0:
+            count+=1
+            lst.append(num)
+    print(lst)         
+    return count
+
+
+# print('simple_list', simple_list(list=[5, 7, 101]))
+
+
+# Напишите функцию, удаляющую из списка целых
+# некоторое заданное число. Из функции нужно вернуть
+# количество удаленных элементов.
+
+def remove_number(list, number):
+    if not number in list:
+        print(list)
+        return 0
+    list.remove(number)
+    return 1 + remove_number(list, number)
+
+print('remove_number', remove_number([5, 101, 7, 101], 101))
+
+def extend_lists(list1:list, list2:list)->list:
+    return list1+list2
+
+# print(extend_lists([1,2,3], [9,8,7]))
+
+def degree_number_list(degree=1, list=[]):
+    new_list = []
+
+    for i in list:
+        new_list.append(i**degree)
+
+    return new_list
+
+
+# print(degree_number_list(2, [2,3,4]))
+
+
+# Написать рекурсивную функцию нахождения наи-
+# большего общего делителя двух целых чисел.
